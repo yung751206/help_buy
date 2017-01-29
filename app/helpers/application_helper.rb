848f81ab63dd176fb6d97be7924fe5660566e8f7
yung751206@gmail.com <yung751206@gmail.com>
2017-01-28 16:50:04 -0800

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
-module ApplicationHelper
+module ApplicationHelperq
+	def full_title(page_title = '')
+		base_title = "美國代購by娜塔莉"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
