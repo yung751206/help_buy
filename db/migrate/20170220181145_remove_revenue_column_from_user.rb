2f5cbfbaa9e14ace5880967b960d0b33e219c413
yung751206@gmail.com <yung751206@gmail.com>
2017-02-20 11:35:26 -0800

    fix migration problem

--- db/migrate/20170220181145_remove_revenue_column_from_user.rb
+++ db/migrate/20170220181145_remove_revenue_column_from_user.rb
@@ -1,6 +1,5 @@
 class RemoveRevenueColumnFromUser < ActiveRecord::Migration
   def change
-  	remove_column :users, :revenue
   	remove_column :orders, :where
   	add_column :orders, :revenue, :decimal
   end
