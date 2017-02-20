60c8583abc275b64f1676fcd79c16df75be31680
yung751206@gmail.com <yung751206@gmail.com>
2017-02-19 22:03:06 -0800

    change default order index sort column

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
 	belongs_to :user
-	default_scope -> { order(created_at: :desc) }
+	default_scope -> { order(updated_at: :desc) }
   validates :user_id, presence: true
 
 	FACEBOOK = 'Facebook'
